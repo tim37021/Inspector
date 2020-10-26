806602d6784e7c85068487c5229609b9b3d483fb
tim37021 <tim37021@gmail.com>
2020-10-26 19:17:33 +0800

    Removed try block

--- PluginLoader.py
+++ PluginLoader.py
@@ -18,18 +18,15 @@ class PythonPluginLoader(object):
         from PySide2.QtQml import qmlRegisterType
         import importlib
 
-        try:
-            pkg = importlib.import_module(self._path)
-            for export in pkg.qmlexports:
-                qmlRegisterType(
-                    export['class'],
-                    export['uri'],
-                    export['major'],
-                    export['minor'],
-                    export['exportName']
-                )
-        except:
-            pass
+        pkg = importlib.import_module(self._path)
+        for export in pkg.qmlexports:
+            qmlRegisterType(
+                export['class'],
+                export['uri'],
+                export['major'],
+                export['minor'],
+                export['exportName']
+            )
             
     @property
     def uri(self):
