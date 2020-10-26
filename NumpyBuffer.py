b58923be1866c6016b19649ac32a895b4d0c53cb
tim37021 <tim37021@gmail.com>
2020-10-26 13:56:43 +0800

    Removed unnecessary imports

--- NumpyBuffer.py
+++ NumpyBuffer.py
@@ -1,10 +1,6 @@
 import numpy as np
 
 from PySide2.QtCore import *
-from PySide2.QtWidgets import *
-from PySide2.QtQml import *
-from PySide2.QtGui import *
-from PySide2.QtQuick import *
 
 class NumpyBuffer(QObject):
     update = Signal()
