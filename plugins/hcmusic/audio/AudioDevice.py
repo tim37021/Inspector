9144f4755d737e3372d3cab49da8f8f2d4e2462f
tim37021 <tim37021@gmail.com>
2020-12-03 16:34:29 +0800

    Finish new signal processing framework

--- plugins/hcmusic/audio/AudioDevice.py
+++ plugins/hcmusic/audio/AudioDevice.py
@@ -4,8 +4,9 @@ AudioDevice plugin is an example plugin, it provides audio functionalities for I
 
 """
 
-from PySide2.QtCore import QObject, Signal, Property, Slot, QByteArray
+from PySide2.QtCore import QObject, Signal, Property, Slot, QByteArray, QAbstractListModel, Qt, QModelIndex
 from PySide2.QtWidgets import QApplication
+from PySide2.QtQuick import QQuickItem
 
 import pyaudio
 import numpy as np
@@ -13,6 +14,260 @@ import queue
 from Buffer import BufferedSource
 
 p = pyaudio.PyAudio()
+
+
+class SignalOutput(QObject):
+    """Output of a Node
+
+    The name of this class may change
+
+    it stores raw byte in [channel, length], row major
+    """
+    changed = Signal()
+
+    def __init__(self, length, channels, parent=None):
+        QObject.__init__(self, parent)
+        self._length = length
+        self._channels = channels
+        self._buf = QByteArray(length * channels * 4, 0)
+
+    def update(self, arr):
+        self.array[...] = arr
+        self.changed.emit()
+
+    @Property(int)
+    def length(self):
+        return self._length
+
+    @Property(int)
+    def channels(self):
+        return self._channels
+
+    @Property(QByteArray, notify=changed)
+    def buffer(self):
+        return self._buf
+
+    @property
+    def array(self):
+        return np.frombuffer(memoryview(self._buf), dtype=np.float32).reshape(
+            self._channels, self._length
+        )
+
+
+class AudioInputDeviceModel(QAbstractListModel):
+    NameRole = Qt.UserRole + 1
+    IndexRole = Qt.UserRole + 2
+    MaxInputChannels = Qt.UserRole + 3
+    DefaultSampleRate = Qt.UserRole + 4
+
+    def __init__(self, parent=None):
+        QAbstractListModel.__init__(self, parent)
+
+        self._entries = []
+
+    def data(self, index, role=Qt.DisplayRole):
+        if 0 <= index.row() < self.rowCount() and index.isValid():
+            item = self._entries[index.row()]
+            if role == AudioInputDeviceModel.NameRole:
+                return item["name"]
+            elif role == AudioInputDeviceModel.IndexRole:
+                return item["index"]
+            elif role == AudioInputDeviceModel.MaxInputChannels:
+                return item["maxInputChannels"]
+            elif role == AudioInputDeviceModel.DefaultSampleRate:
+                return item["defaultSampleRate"]
+
+    def rowCount(self, parent=QModelIndex()):
+        if parent.isValid():
+            return 0
+        return len(self._entries)
+
+    def roleNames(self):
+        roles = dict()
+        roles[AudioInputDeviceModel.NameRole] = b"name"
+        roles[AudioInputDeviceModel.IndexRole] = b"deviceIndex"
+        roles[AudioInputDeviceModel.MaxInputChannels] = b"maxInputChannels"
+        roles[AudioInputDeviceModel.DefaultSampleRate] = b"defaultSampleRate"
+        return roles
+
+    def update(self, index, name, maxInputChannels, defaultSampleRate):
+        # check if it exist in the list
+        idx = [i for i, x in enumerate(self._entries) if x['index'] == index]
+        idx = idx[0] if len(idx) > 0 else None
+        if idx is None:
+            self.beginInsertRows(QModelIndex(), self.rowCount(), self.rowCount())
+            self._entries.append({
+                'index': index,
+                'name': name,
+                'maxInputChannels': maxInputChannels,
+                'defaultSampleRate': defaultSampleRate
+            })
+            self.endInsertRows()
+        else:
+            self._entries[idx]['name'] = name
+            self._entries[idx]['maxInputChannels'] = maxInputChannels
+            self._entries[idx]['defaultSampleRate'] = defaultSampleRate
+            self.dataChanged.emit(QModelIndex(idx), QModelIndex(idx))
+
+    def remove(self, indices):
+        indices = sorted(indices)
+        rm = 0
+
+        for index in indices:
+            self.beginRemoveRows(QModelIndex(), index-rm, index-rm)
+            self._entries.pop(index-rm)
+            self.endRemoveRows()
+
+    @property
+    def availableIndices(self):
+        return [x.index for x in self._entries]
+
+
+class AudioDiscoveryModelProvider(QAbstractListModel):
+    inputDeviceModelChanged = Signal()
+    defaultInputDeviceIndexChanged = Signal()
+    defaultOutputDeviceIndexChanged = Signal()
+
+    def __init__(self, parent=None):
+        QAbstractListModel.__init__(self, parent)
+
+        self._inputModel = AudioInputDeviceModel(self)
+        self.refresh()
+
+    @Property(QObject, final=True, notify=inputDeviceModelChanged)
+    def inputDeviceModel(self):
+        return self._inputModel
+
+    @Property(int, final=True, notify=defaultInputDeviceIndexChanged)
+    def defaultInputDeviceIndex(self):
+        return p.get_default_input_device_info()['index']
+
+    @Property(int, final=True, notify=defaultOutputDeviceIndexChanged)
+    def defaultOutputDeviceIndex(self):
+        return p.get_default_output_device_info()['index']
+
+    @Slot()
+    def refresh(self):
+        c = p.get_device_count()
+
+        inp = []
+        for i in range(c):
+            dev = p.get_device_info_by_index(i)
+            if dev['maxInputChannels'] > 0:
+                self._inputModel.update(
+                    dev['index'],
+                    dev['name'],
+                    dev['maxInputChannels'],
+                    dev['defaultSampleRate']
+                )
+                inp.append(dev['index'])
+
+
+class AudioInputDevice2(QQuickItem):
+    bufferLengthChanged = Signal()
+    rateChanged = Signal()
+    activeChanged = Signal()
+    deviceIndexChanged = Signal()
+    outputChanged = Signal()
+
+    def __init__(self, parent=None):
+        QQuickItem.__init__(self, parent)
+
+        self._bufferLength = 1024
+        self._output = SignalOutput(1024, 1)
+        self._active = False
+        self._rate = 44100
+        self._deviceIndex = p.get_default_input_device_info()['index']
+        self._stream = None
+        self._inited = False
+        QApplication.instance().aboutToQuit.connect(lambda: self.closeDevice())
+
+    @Property(SignalOutput, notify=outputChanged)
+    def output(self):
+        return self._output
+
+    @Property(bool)
+    def active(self):
+        return self._active
+
+    @active.setter
+    def active(self, val):
+        if self._active != val:
+            self._active = val
+            self.activeChanged.emit()
+
+            if self._inited and self._active:
+                self.openDevice()
+            if self._inited and not self._active:
+                self.closeDevice()
+
+    @Property(int)
+    def bufferLength(self):
+        return self._bufferLength
+
+    @bufferLength.setter
+    def bufferLength(self, val):
+        if self._bufferLength != val:
+            self._bufferLength = val
+            self.bufferLengthChanged.emit()
+            self._output = SignalOutput(val, 1)
+            self.outputChanged.emit()
+
+            if self._inited and self._active:
+                raise Exception('Changing bufferLength when active is not support yet')
+
+    @Property(int)
+    def rate(self):
+        return self._rate
+
+    @rate.setter
+    def rate(self, val):
+        if self._rate != val:
+            self._rate = val
+            self.rateChanged.emit()
+
+            if self._inited and self._active:
+                self.openDevice()
+
+    @Property(int)
+    def deviceIndex(self):
+        return self._deviceIndex
+
+    @deviceIndex.setter
+    def deviceIndex(self, val):
+        if self._deviceIndex != val:
+            self._deviceIndex = val
+            self.deviceIndexChanged.emit()
+
+            if self._inited and self._active:
+                self.openDevice()
+
+    def componentComplete(self):
+        self._inited = True
+        if self._active:
+            self.openDevice()
+
+    def openDevice(self):
+        self.closeDevice()
+        self._stream = p.open(format=pyaudio.paInt16,
+            channels=1,
+            rate=self._rate,
+            input=True,
+            output=False,
+            frames_per_buffer=self._bufferLength,
+            input_device_index=self._deviceIndex,
+            stream_callback=self.callback
+        )
+        self._stream.start_stream()
+
+    def closeDevice(self):
+        if self._stream is not None:
+            self._stream.close()
+
+    def callback(self, in_data, frame_count, time_info, status):
+        buf = np.frombuffer(in_data, dtype=np.int16).astype(np.float32).reshape(1, -1)
+        self._output.update(buf)
+        return (None, pyaudio.paContinue)
 
 
 class AudioInputDevice(BufferedSource):
@@ -194,6 +449,147 @@ class AudioOutputDevice(QObject):
             self._q.put(np.pad(b, (0, 1024-len(b))))
             i += 1024
         self._stream.start_stream()
+
+    def callback(self, in_data, frame_count, time_info, status):
+        if not self._q.empty():
+            data = self._q.get_nowait()
+            if data is not None:
+                return (data, pyaudio.paContinue)
+        else:
+            return (np.zeros(1024, dtype=np.int16), pyaudio.paContinue)
+
+
+class AudioOutputDevice2(QQuickItem):
+    """Audio Output Device DSP Style
+
+    Limitations:
+    TODO Input buffer should be larger
+    """
+    activeChanged = Signal()
+    bufferLengthChanged = Signal()
+    deviceIndexChanged = Signal()
+    inputChanged = Signal()
+    rateChanged = Signal()
+
+    def __init__(self, parent=None):
+        QQuickItem.__init__(self, parent)
+        self._rate = 44100
+        self._deviceIndex = p.get_default_output_device_info()['index']
+        self._bufferLength = 1024
+        self._input = None
+        self._active = False
+        self._stream = None
+        self._inited = False
+        import queue
+        self._q = queue.Queue()
+
+        QApplication.instance().aboutToQuit.connect(lambda: self.closeDevice())
+
+    @Property(int)
+    def rate(self):
+        return self._rate
+
+    @rate.setter
+    def rate(self, val):
+        if self._rate != val:
+            self._rate = val
+            self.rateChanged.emit()
+
+            if self._inited and self._active:
+                self.openDevice()
+
+    @Property(int)
+    def deviceIndex(self):
+        return self._deviceIndex
+
+    @deviceIndex.setter
+    def deviceIndex(self, val):
+        if self._deviceIndex != val:
+            self._deviceIndex = val
+            self.deviceIndexChanged.emit()
+
+            if self._inited and self._active:
+                self.openDevice()
+
+    @Property(int)
+    def bufferLength(self):
+        return self._bufferLength
+
+    @bufferLength.setter
+    def bufferLength(self, val):
+        if self._bufferLength != val:
+            self._bufferLength = val
+            self.bufferLengthChanged.emit()
+
+            if self._inited:
+                raise Exception('Changing buffer size on the fly is not support yet')
+
+    @Property(SignalOutput)
+    def input(self):
+        return self._input
+
+    @input.setter
+    def input(self, val):
+        if self._input != val:
+            if self._input is not None:
+                self._input.changed.disconnect(self.appendBuffer)
+
+            self._input = val
+            self._input.changed.connect(self.appendBuffer)
+            self.inputChanged.emit()
+
+    @Property(bool)
+    def active(self):
+        return self._active
+
+    @active.setter
+    def active(self, val):
+        if self._active != val:
+            self._active = val
+            self.activeChanged.emit()
+
+            if self._inited and self._active:
+                self.openDevice()
+            if self._inited and not self._active:
+                self.closeDevice()
+
+    def appendBuffer(self):
+        """append buffer when input buffer changed
+
+        TODO Discussion: should we auto normalize the buffer?
+        """
+        data = self._input.array
+        if (np.abs(data) > 16384).any():
+            data /= data.max()
+            data *= 16384
+        data = data.astype(np.int16)
+        i = 0
+        bl = self._bufferLength
+        while i < self._input._length:
+            b = data[..., i: i+bl]
+            self._q.put(np.pad(b, (0, bl-len(b))))
+            i += bl
+
+    def componentComplete(self):
+        self._inited = True
+        if self._active:
+            self.openDevice()
+
+    def openDevice(self):
+        self.closeDevice()
+        self._stream = p.open(format=pyaudio.paInt16,
+                        channels=1,
+                        rate=self._rate,
+                        output=True,
+                        frames_per_buffer=self._bufferLength,
+                        input_device_index=self._deviceIndex,
+                        stream_callback=self.callback
+        )
+        self._stream.start_stream()
+
+    def closeDevice(self):
+        if self._stream is not None:
+            self._stream.close()
 
     def callback(self, in_data, frame_count, time_info, status):
         if not self._q.empty():
--- plugins/hcmusic/audio/__init__.py
+++ plugins/hcmusic/audio/__init__.py
@@ -1,6 +1,11 @@
-from .AudioDevice import AudioInputDevice, AudioOutputDevice
+from .AudioDevice import AudioInputDevice, AudioInputDevice2, AudioOutputDevice, AudioOutputDevice2
+from .AudioDevice import AudioDiscoveryModelProvider
+
 
 qmlexports = [
+    {'class': AudioDiscoveryModelProvider, 'exportName': 'AudioDiscoveryModelProvider', 'uri': 'hcmusic.audio', 'major': 1, 'minor': 0},
     {'class': AudioInputDevice, 'exportName': 'AudioInputDevice', 'uri': 'hcmusic.audio', 'major': 1, 'minor': 0},
+    {'class': AudioInputDevice2, 'exportName': 'AudioInputDevice2', 'uri': 'hcmusic.audio', 'major': 1, 'minor': 0},
     {'class': AudioOutputDevice, 'exportName': 'AudioOutputDevice', 'uri': 'hcmusic.audio', 'major': 1, 'minor': 0},
+    {'class': AudioOutputDevice2, 'exportName': 'AudioOutputDevice2', 'uri': 'hcmusic.audio', 'major': 1, 'minor': 0},
 ]
