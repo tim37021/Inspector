90bf9412e177b62532556b70eb0c1aea2db15bb8
ps2747 <ps2747@gmail.com>
2021-04-09 10:30:39 +0800

    Added channel info to Csvloader

--- plugins/hcmusic/loader/CsvLoader.py
+++ plugins/hcmusic/loader/CsvLoader.py
@@ -1,4 +1,4 @@
-from PySide2.QtCore import QUrl, Signal, Property
+from PySide2.QtCore import QUrl, Signal, Property, Slot
 from PySide2.QtWidgets import QApplication
 from ..dsp.Node import QtSignal1D, Node, Signal1D
 
@@ -11,6 +11,12 @@ class QCsvLoader(Node):
     def __init__(self, parent=None):
         Node.__init__(self, parent)
         self._header = []
+        self._channelVUnits = []
+        self._channelHUnits = []
+        self._channelDates = []
+        self._channelTimes = []
+        self._channelHResolution = []
+        self._channelHOffset = []
         self._output = QtSignal1D()
         self._filename = ''
 
@@ -31,6 +37,34 @@ class QCsvLoader(Node):
     @Property(Signal1D, final=True, notify=outputChanged)
     def output(self):
         return self._output
+    
+    @Slot(int, result=str)
+    def getChannelVUnit(self, ch):
+        return self._channelVUnits[ch]
+
+    @Slot(int , result=str)
+    def getChannelHUnit(self, ch):
+        return self._channelHUnits[ch]
+
+    @Slot(int, result=str)
+    def getChannelDate(self, ch):
+        return self._channelDates[ch]
+
+    @Slot(int, result=str)
+    def getChannelTime(self, ch):
+        return self._channelTimes[ch]
+
+    @Slot(int, result=str)
+    def getChannelHResolution(self, ch):
+        return self._channelHResolution[ch]
+
+    @Slot(int, result=str)
+    def getChannelHOffset(self, ch):
+        return self._channelHOffset[ch]
+
+    @Slot(int, result=str)
+    def getChannelHUnit(self, ch):
+        return self._channelHUnits[ch]
 
     def close(self):
         pass
@@ -39,12 +73,34 @@ class QCsvLoader(Node):
         self._output.alloc(0, 6)
 
     def _extractFile(self, filename):
-        self._header = np.genfromtxt(filename, delimiter = ',', skip_header = 2, max_rows = 8, dtype = str) 
-        buf =np.genfromtxt(filename, delimiter = ',', skip_header = 10, usecols = (1, 2, 3, 4, 5, 6))
+        header = np.genfromtxt(filename, delimiter = ',', skip_header = 2, max_rows = 8, dtype = str) 
+        # Get channels count
+        channels = 0
+        for block in header[0]:
+            if "CH" in block:
+                channels += 1
+
+        # Clear channel info
+        self._channelVUnits = []
+        self._channelHUnits = []
+        self._channelDates = []
+        self._channelTimes = []
+        self._channelHResolution = []
+        self._channelHOffset = []
+
+        # Set channel info
+        for i in range(1, channels + 1):
+            self._channelDates.append(header[2][i])
+            self._channelTimes.append(header[3][i])
+            self._channelVUnits.append(header[4][i])
+            self._channelHResolution.append(header[5][i])
+            self._channelHOffset.append(header[6][i])
+            self._channelHUnits.append(header[7][i])
+
+        # Load channel data
+        buf =np.genfromtxt(filename, delimiter = ',', skip_header = 10, usecols = range(1, channels + 1))
         length, _ = buf.shape
         self._output.resize(length)
-        print(length)
         self._output.numpy_array[:, :] = buf[:, :]
-        print(self._output.numpy_array)
         if length > 0:
             self._output.update.emit(0, length)
